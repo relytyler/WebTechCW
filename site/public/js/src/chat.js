8b3758cd89993ac96fcdd1820fe1d9ea0917a4dc
roberto <roberto.lausoto@gmail.com>
2020-05-31 12:09:49 +0100

    watch page

---
+++ site/public/js/src/chat.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/../index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(8080, () => {
+  console.log('listening on *:8080');
+});
+
+
+
---
+++ site/public/js/watch.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var videos = document.getElementsByClassName("youtube");
+
+for (var i = 0; i < videos.length; i++) {
+
+    var youtube = videos[i];
+
+    var img = document.createElement('img');
+    img.setAttribute("src", "http://i.ytimg.com/vi/" + youtube.id + "/hqdefault.jpg");
+    img.setAttribute("class", "thumb");
+
+    var circle = document.createElement("div");
+    circle.setAttribute("class", "circle");
+
+    youtube.appendChild(img);
+    youtube.appendChild(circle);
+
+    youtube.onclick = function () {
+
+        var iframe = document.createElement("iframe");
+        iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1");
+
+        iframe.style.width = this.style.width;
+        iframe.style.height = this.style.height;
+
+        this.parentNode.replaceChild(iframe, this);
+    };
+}
